7650ba417c62b6da562426f54ae99d70753cdaf6
Siwoo Jeon <me@migan.co.kr>
2026-02-27 22:41:29 +0900

    fix: style

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -13,7 +13,7 @@
 <input
 	type="text"
 	class={`border border-black w-20 h-20 text-center
-	${main ? "bg-blue-500 text-white outline-none ring-0" : null}
+	${main ? "bg-blue-500 text-white" : null}
 	${sub ? "bg-yellow-500 text-white" : null}`}
 	bind:value
 />
