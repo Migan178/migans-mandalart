a860df29cd255a6e2e18a397e00b53c10da6cd2f
Siwoo Jeon <me@migan.co.kr>
2026-03-05 18:50:55 +0900

    fix: auto line change

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -14,10 +14,10 @@
 	}
 </script>
 
-<input
-	type="text"
+<div
+	contenteditable="true"
 	class={`h-10 w-10 text-center text-[8px] md:h-20 md:w-20 md:text-base
 	${sub ? "bg-yellow-300 text-white dark:bg-mauve-900" : null}`}
 	onblur={handleBlur}
-	bind:value
-/>
+	bind:textContent={value}
+></div>
